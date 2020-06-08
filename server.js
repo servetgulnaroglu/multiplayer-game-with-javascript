b9d1cd791cd759d3aff4b7b195638053507082db
servetgulnaroglu <servet.gulnaroglu@gmail.com>
2020-06-08 21:14:39 +0300

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 const serverless = require('serverless-http');
 var app = express();
 
-var server = app.listen(3000);
+var server = app.listen(80);
 app.use(express.static('public'));//folder name = public 
 var socket = require('socket.io');
 var io = socket(server);
